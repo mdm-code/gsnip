36581cc016c9d3ca11b3c5eb4fc9956a1206549a
mdm-code <code.madamczyk@gmail.com>
2021-08-11 11:05:48 +0200

    [FEAT] Made finite state automaton scanner
    This is a rather naive implementation, but
    it should do for now.
    
    It scans the text and follows a predefined
    syntax.

--- main.go
+++ main.go
@@ -1,18 +1,75 @@
 package main
 
-import "os"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func main() {
+	f, err := os.Open("assets/sample.snip")
+	defer f.Close()
+	if err != nil {
+		os.Exit(1)
+	}
+	fmt.Println(parse(f))
 }
 
-type snippet struct {
+type Snip struct {
 	name, desc, body string
 }
 
-func parse(f *os.File) (snippet, error) {
-	return snippet{
-		name: "func",
-		desc: "Go function with no attributes and returns",
-		body: "func namedFunction() {\n\treturn\n}",
-	}, nil
+func (s Snip) String() string {
+	return fmt.Sprintf("%s\n%s\n\n%s", s.name, s.desc, s.body)
+}
+
+type State uint8
+
+const (
+	SCANLINE State = iota
+	SIGNATURE
+	SCANBODY
+)
+
+func parse(f *os.File) (map[string]Snip, error) {
+	// Finite state automaton for reading file contents.
+	in := bufio.NewScanner(f)
+	snips := make(map[string]Snip)
+	state := SCANLINE
+
+	var line string
+	var name, desc string
+	var body []string
+
+	for in.Scan() {
+		if state == SCANLINE {
+			line = in.Text()
+			if strings.HasPrefix(line, "startsnip") {
+				state = SIGNATURE
+			}
+		}
+		if state == SCANBODY {
+			line = in.Text()
+			if strings.HasPrefix(line, "endsnip") {
+				snips[name] = Snip{
+					name: name,
+					desc: desc,
+					body: strings.Join(body, "\n"),
+				}
+				state = SCANLINE
+				body = body[:0]
+				continue
+			}
+			body = append(body, line)
+		}
+		if state == SIGNATURE {
+			elems := strings.SplitN(line, " ", 3)
+			name = elems[1]
+			desc = strings.Trim(elems[2], "\"")
+			state = SCANBODY
+			continue
+		}
+	}
+	return snips, nil
 }
--- main_test.go
+++ main_test.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"reflect"
 	"testing"
 )
 
+var funcSnip = Snip{
+	name: "func",
+	desc: "Go function with no attributes and returns",
+	body: "func namedFunction() {\n\treturn\n}",
+}
+
+var structSnip = Snip{
+	name: "struct",
+	desc: "Go struct template",
+	body: `type namedStruct struct {
+	name string
+	id int
+}`,
+}
+
+var snips = map[string]Snip{"func": funcSnip, "struct": structSnip}
+
 func TestParsing(t *testing.T) {
-	f, err := os.Open("assets/sample.snippet")
+	f, err := os.Open("assets/sample.snip")
 	if err != nil {
-		t.Error("test failed when opening snippet file")
+		t.Error("test failed when opening file with snippets")
 	}
 	defer f.Close()
 	has, err := parse(f)
-	want := snippet{
-		name: "func",
-		desc: "Go function with no attributes and returns",
-		body: "func namedFunction() {\n\treturn\n}",
+	if ok := reflect.DeepEqual(has, snips); !ok {
+		t.Errorf("want: %s; has %s", snips, has)
 	}
+}
+
+func TestSnipStructDisplay(t *testing.T) {
+	want := `func
+Go function with no attributes and returns
+
+func namedFunction() {
+	return
+}`
+	has := fmt.Sprintf("%s", funcSnip)
 	if has != want {
-		t.Errorf("want: %s; has %s", want, has)
+		t.Errorf("has: %s; want: %s", has, want)
 	}
 }

af7d168cc1a354006ac3d9b1c85e5a11779db3d4
mdm-code <code.madamczyk@gmail.com>
2021-08-18 15:47:59 +0200

    [FEAT] Written up the entrypoint cmd for gsnip
    This is still WiP pretty much, but it's an honest
    interface .

--- cmd/gsnip/main.go
+++ cmd/gsnip/main.go
@@ -1,25 +1,75 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/mdm-code/gsnip/parsing"
 )
 
+const Source = "/usr/local/share/gsnip/snippets"
+
 func main() {
-	var file string
-	flag.StringVar(&file, "snippets", "", "Flat file with snippets")
+	fn := flag.String("f", Source, "Snippets source file")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Snippet manager written in Go.\n\nUsage:\n")
+		flag.PrintDefaults()
+	}
 	flag.Parse()
-	f, err := os.Open(file)
+
+	var f *os.File
+	f, err := os.Open(*fn)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal("Could not open " + *fn)
+		f, err = os.Open(Source)
+		if err != nil {
+			log.Fatal("Could not find " + Source)
+		}
 	}
+	defer f.Close()
+
 	parser := parsing.NewParser()
-	result, err := parser.Parse(f)
+	snippets, err := parser.Parse(f)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-	fmt.Println(result)
+
+	// Check if there's anything to read on STDIN
+	isPiped := func() bool {
+		fi, _ := os.Stdin.Stat()
+		return (fi.Mode() & os.ModeCharDevice) == 0
+	}
+
+	if isPiped() {
+		s := bufio.NewScanner(os.Stdin)
+		s.Split(bufio.ScanWords)
+		var attrs []string
+		for s.Scan() {
+			attrs = append(attrs, s.Text())
+		}
+
+		var search string
+		var repls []string
+		if len(attrs) == 0 {
+			log.Fatal("There's nothing to find")
+		} else if len(attrs) == 1 {
+			search = attrs[0]
+		} else {
+			search, repls = attrs[0], attrs[1:]
+		}
+
+		snip, ok := snippets[search]
+		if !ok {
+			os.Stderr.WriteString(search + " was not found")
+		}
+		pat := `\${[0-9]+:\w*}`
+		out, ok := parsing.Replace(snip.Body, pat, repls...)
+		if !ok {
+			os.Stderr.WriteString("Failed to compile regex pattern: " + pat)
+		}
+		os.Stdout.WriteString(out)
+	}
 }

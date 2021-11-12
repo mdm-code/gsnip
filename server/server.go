a8a4fa61647f414889792e4ebce0245953f0ed99
mdm-code <code.madamczyk@gmail.com>
2021-11-12 15:20:22 +0100

    [FEAT] Embedded Interpreter into the Server
    The object responsible for evaluating input is
    now embedded into the Server structure.

--- server/server.go
+++ server/server.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 
 	"github.com/mdm-code/gsnip/manager"
+	"github.com/mdm-code/gsnip/signals"
 )
 
 type Logger interface {
@@ -39,12 +40,13 @@ type Server interface {
 }
 
 type UDPServer struct {
-	addr  net.UDPAddr
-	conn  *net.UDPConn
-	mngr  *manager.Manager
-	sigs  chan os.Signal
-	logr  Logger
-	fname string
+	addr   net.UDPAddr
+	conn   *net.UDPConn
+	mngr   *manager.Manager
+	sigs   chan os.Signal
+	logr   Logger
+	interp signals.Interpreter
+	fname  string
 }
 
 func NewServer(ntwrk string, addr string, port int, fname string) (Server, error) {
@@ -61,7 +63,6 @@ func NewServer(ntwrk string, addr string, port int, fname string) (Server, error
 }
 
 func NewUDPServer(addr string, port int, fname string) (*UDPServer, error) {
-	// TODO: Instantiate evaluator here and pass it to both Manager & Server
 	m, err := manager.NewManager(fname)
 	if err != nil {
 		return nil, err
@@ -71,10 +72,11 @@ func NewUDPServer(addr string, port int, fname string) (*UDPServer, error) {
 			IP:   net.ParseIP(addr),
 			Port: port,
 		},
-		mngr:  m,
-		sigs:  make(chan os.Signal, 1),
-		logr:  NewLogger(),
-		fname: fname,
+		mngr:   m,
+		sigs:   make(chan os.Signal, 1),
+		logr:   NewLogger(),
+		interp: signals.NewInterpreter(),
+		fname:  fname,
 	}, nil
 }
 
@@ -121,10 +123,9 @@ func (s *UDPServer) AwaitConn() {
 }
 
 func (s *UDPServer) respond(addr *net.UDPAddr, buff []byte) {
-	inMsg := string(buff)
-	// TODO: Evaluate signal here
-	switch inMsg {
-	case "@RELOAD":
+	token := s.interp.Eval(string(buff))
+	switch token.IsReload() {
+	case true:
 		s.sigs <- syscall.SIGHUP
 		_, err := s.conn.WriteToUDP([]byte(""), addr)
 		if err != nil {
@@ -133,7 +134,7 @@ func (s *UDPServer) respond(addr *net.UDPAddr, buff []byte) {
 		}
 		return
 	default:
-		resp, err := s.mngr.Execute(inMsg)
+		resp, err := s.mngr.Execute(token)
 		if err != nil {
 			s.logr.Log("ERROR", err)
 			_, err = s.conn.WriteToUDP([]byte("ERROR"), addr)

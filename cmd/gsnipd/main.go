e9d5f874c8dab08a0f40dcff667071b2b0e90f59
mdm-code <code.madamczyk@gmail.com>
2021-11-13 11:46:02 +0100

    [MAINT] Removed redundant variables in gsnipd

--- cmd/gsnipd/main.go
+++ cmd/gsnipd/main.go
@@ -15,15 +15,12 @@ var (
 	port int
 	addr string
 	file string
-	prot string
-	sigs chan os.Signal
 )
 
 func main() {
 	flag.IntVar(&port, "port", 7862, "UDP server port")
 	flag.StringVar(&addr, "addr", "127.0.0.1", "UDP server IP address")
 	flag.StringVar(&file, "file", source, "snippet source file")
-	flag.StringVar(&prot, "protocol", "udp", "internet protocol")
 	setupFlags(flag.CommandLine)
 	flag.Parse()
 

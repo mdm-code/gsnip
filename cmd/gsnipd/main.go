37dfd754a447a134347282c37d6b2343be65b98b
mdm-code <code.madamczyk@gmail.com>
2022-01-06 19:47:00 +0100

    [FEAT] gsnipd command handles UDS server

--- cmd/gsnipd/main.go
+++ cmd/gsnipd/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"path"
-	"strconv"
 	"syscall"
 
 	"github.com/mdm-code/gsnip/server"
@@ -13,14 +13,12 @@ import (
 )
 
 var (
-	port int
-	addr string
+	sock string
 	file string
 )
 
 func main() {
-	flag.IntVar(&port, "port", 7862, "UDP server port")
-	flag.StringVar(&addr, "addr", "127.0.0.1", "UDP server IP address")
+	flag.StringVar(&sock, "sock", "/tmp/gsnip.sock", "UDS server socket name")
 	flag.StringVar(&file, "file", "", "snippet source file")
 	setupFlags(flag.CommandLine)
 	flag.Parse()
@@ -29,13 +27,19 @@ func main() {
 		dirs := xdg.Arrange()
 		dir, ok := xdg.Discover(dirs)
 		if !ok {
-			fmt.Fprintf(os.Stderr, "gsnipd ERROR: could not find any snippet file")
+			fmt.Fprintf(
+				os.Stderr,
+				"gsnipd ERROR: could not find any snippet file",
+			)
 			os.Exit(1)
 		}
 		file = path.Join(dir.Item(), "snippets")
 	}
 
-	s, err := server.NewServer("udp", addr, port, file)
+	cleanup()
+	defer cleanup()
+
+	s, err := server.NewServer("unix", sock, file)
 	defer s.ShutDown()
 
 	if err != nil {
@@ -43,12 +47,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	s.Log("INFO", "reading source file: "+file)
+	s.Log("INFO", fmt.Sprintf("reading source file: %s", file))
 	err = s.Listen()
 	if err != nil {
 		s.Log(
 			"ERROR",
-			fmt.Sprintf("UDP address taken: %s:%s", addr, strconv.Itoa(port)),
+			fmt.Sprintf("UDS socket file taken: %s", sock),
 		)
 		os.Exit(2)
 	}
@@ -61,5 +65,13 @@ func setupFlags(f *flag.FlagSet) {
 		fmt.Fprintf(f.Output(), "Usage of %s:\n\n", os.Args[0])
 		fmt.Fprintf(f.Output(), "Start the snippet server.\n\n")
 		f.PrintDefaults()
+	}
+}
+
+func cleanup() {
+	if _, err := os.Stat(sock); err == nil {
+		if err := os.RemoveAll(sock); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
